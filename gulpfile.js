40ea9bfb1f37c8a448c095654d9ba637086650da
renzo2099 <aliendrorenzo4@gmail.com>
2024-07-22 03:28:14 -0300

    Primera Versión de mi Portafolio Personal - 22/07/2024 (03:28).

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+
+// CSS y SASS
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
+const plumber = require('gulp-plumber');
+
+// Función que nos permite compilar el archivo SASS.
+function css(done) {
+  // Compilamos SASS
+
+  // 1. Identificamos el archivo (hoja de SASS).
+  // 2. Compilamos el archivo.
+  // 3. Guardamos el archivo ".css".
+
+  // Le indicamos el archivo para que pueda identificarlo:
+    src('src/scss/app.scss')
+    // Prevenir que los errores detengan la compilación
+      .pipe(plumber({
+        errorHandler: function(err) {
+          console.error('Error en la compilación de SASS:', err.message);
+          this.emit('end');
+        }
+      }))
+
+        // Inicializamos la creación del sourcemap para nuestra hooja de Sass
+        .pipe( sourcemaps.init() )
+
+        // Compilamos el archivo.
+        .pipe(sass())
+        .pipe(postcss([
+          // Función en concreto para comprimir el archivo .css:
+          autoprefixer(), cssnano()
+          
+          // autoprefixer()
+        ]))
+
+        // El sourcemap se guardará en el mismo lugar que la hoja de Sass gracias al "."
+        .pipe( sourcemaps.write('.'))
+
+        // Y por último, lo almacenamos en el proyecto.
+        .pipe(dest("build/css"));
+
+    done();
+}
+
+// Función que nos permite compilar en tiempo real el archivo SASS.
+function dev() {
+  // La función watch() toma como argumentos dos argumentos:
+  // El primero se trata del archivo a inspeccionar y,
+  // en caso de cambios en el mismo,
+  // ejecuta la función que toma como segundo argumento.
+
+  // Sintaxis para múltiples archivos con la extensión ".scss":
+    watch('src/scss/**/*.scss', css);
+}
+
+exports.css = css;
+exports.dev = dev;
+
+// Con series podemos inicializar múltiples tareas, pero primero debe
+// llegar a su final la primera para poder continuar con la segunda tarea.
+
+// Con Parallel podemos realizar múltiples tareas en paralelo, lo que quiere decir
+// que todas las tareas se ejecutarán al mismo tiempo e irán finalizando en su respectivo orden.
+
+// Tareas por default:
+exports.default = series(css, dev);
